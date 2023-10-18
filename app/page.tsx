6fa5506d2990b6c881178d5247bfbfe9299c7325
miguel-merlin <mmerlin@stevens.edu>
2023-10-18 01:36:52 -0400

    feat(map): Added map to Next.js boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-import Image from 'next/image'
+'use client'
+import { MapContainer, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
 
 export default function Home() {
   return (
-    <main></main>
+    <>
+      <MapContainer
+        style={{
+          height: 800,
+          margin: 10,
+          padding: 0,
+          borderRadius: 10,
+          border: "4px solid black",
+          boxShadow: "0 0 20px #ccc",
+        }}
+        center={[40.73061, -73.935242]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </>
   )
 }
