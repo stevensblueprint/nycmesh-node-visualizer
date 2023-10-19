23239b8fbea42d92a49a535ca76e10ffacb71d12
Joshua Gorman <90810000+endevii@users.noreply.github.com>
2023-10-19 00:12:25 -0400

    feaf(map): Added temp code to display antennas

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,43 @@
-'use client'
-import { MapContainer, TileLayer } from 'react-leaflet';
+"use client";
+import { MapContainer, TileLayer, Circle, Popup, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+let access_points = require("../access_points.json");
 
 export default function Home() {
+  function reduceAPs() {
+    let reduced = [];
+    for (let i = 0; i < access_points.length; i++) {
+      reduced.push({
+        lat: access_points[i].location.latitude,
+        lon: access_points[i].location.longitude,
+        name: access_points[i].identification.name,
+      });
+    }
+    return reduced;
+  }
+
+  type AccessPoint = {
+    lat: number;
+    lon: number;
+    name: string;
+  };
+
+  function createAntennas() {
+    let antennas = reduceAPs();
+    return antennas.map((antenna: AccessPoint) => (
+      // circle
+      <Circle
+        center={[antenna.lat, antenna.lon]}
+        radius={100}
+        color="red"
+        fillOpacity={0.5}
+        key={antenna.name}
+      >
+        <Popup>{antenna.name}</Popup>
+      </Circle>
+    ));
+  }
+
   return (
     <>
       <MapContainer
@@ -16,13 +51,14 @@ export default function Home() {
         }}
         center={[40.73061, -73.935242]}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {createAntennas()}
       </MapContainer>
     </>
-  )
+  );
 }
