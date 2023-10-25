a99ad7d10868767fca89ef41a843fb600cfb87db
Joshua Gorman <itssnughere@gmail.com>
2023-10-25 19:24:05 -0400

    feat(map): Split map and antennas into components

---
+++ app/components/Antennas.tsx
@@ -0,0 +1,82 @@
+import { Circle, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+let access_points = require("../../access_points.json");
+
+  // Types are temporary until the API is up and running
+  type AccessPoint = {
+    lat: number;
+    lon: number;
+    name: string;
+  };
+
+  interface ReducedContent {
+    points: [AccessPoint?]
+    lat: number
+    lon: number
+  }
+  
+  interface ReducedPoints {
+    [key: string]: ReducedContent
+  }
+
+export default function Antennas() {
+  // Will be removed once calling the API is up and running
+  function reduceAPs(): [AccessPoint] {
+    let reduced: [AccessPoint] = [{
+      lat: 0,
+      lon: 0,
+      name: "default"
+    }];
+    for (let i = 0; i < access_points.length; i++) {
+      reduced.push({
+        lat: access_points[i].location.latitude,
+        lon: access_points[i].location.longitude,
+        name: access_points[i].identification.name,
+      });
+    }
+    return reduced;
+  }
+
+  let antennas: [AccessPoint] = reduceAPs();
+  let reducedAntennas: ReducedPoints = {};
+
+  for(let i = 0; i < antennas.length; i++) {
+    let val: string = `${antennas[i].lat} ${antennas[i].lon}`
+    if(!(val in reducedAntennas)) {
+      reducedAntennas[val] = {
+        points: [],
+        lat: antennas[i].lat,
+        lon: antennas[i].lon
+      };
+    }
+    reducedAntennas[val]["points"].push(antennas[i]);
+  }
+
+  const convertPoints = (points: [AccessPoint?]): [JSX.Element?] => {
+    let converted: [JSX.Element?] = [];
+    for(let i = 0; i < points.length; i++) {
+      converted.push(<p className="m-0 p-0">{points[i]?.name}</p>);
+    }
+    return converted;
+  }
+
+  return Object.entries(reducedAntennas).map((value: [string, ReducedContent]) => (
+    // Creation of the circle for each antenna
+    <Circle
+      center={[value[1].lat, value[1].lon]}
+      radius={100}
+      color="red"
+      fillOpacity={0.5}
+      key={value[0]}
+    >
+      {/* Create a popup which has the names of all antennas at some node */}
+      return <Popup> 
+          <div className="m-0 p-0">
+            Access Points: {value[1].points.length}
+            {convertPoints(value[1].points)}
+          </div>
+        </Popup>;
+    </Circle>
+  ));
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,16 @@
 "use client";
-import { MapContainer, TileLayer, Circle, Popup, Marker } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-let access_points = require("../access_points.json");
+import dynamic from 'next/dynamic'
+
+// This is requires so that the "no window" error does not occur when loading the map
+const DynamicMap = dynamic(() => import("./components/Map"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+})
 
 export default function Home() {
-  function reduceAPs() {
-    let reduced = [];
-    for (let i = 0; i < access_points.length; i++) {
-      reduced.push({
-        lat: access_points[i].location.latitude,
-        lon: access_points[i].location.longitude,
-        name: access_points[i].identification.name,
-      });
-    }
-    return reduced;
-  }
-
-  type AccessPoint = {
-    lat: number;
-    lon: number;
-    name: string;
-  };
-
-  function createAntennas() {
-    let antennas = reduceAPs();
-    return antennas.map((antenna: AccessPoint) => (
-      // circle
-      <Circle
-        center={[antenna.lat, antenna.lon]}
-        radius={100}
-        color="red"
-        fillOpacity={0.5}
-        key={antenna.name}
-      >
-        <Popup>{antenna.name}</Popup>
-      </Circle>
-    ));
-  }
-
   return (
     <>
-      <MapContainer
-        style={{
-          height: 800,
-          margin: 10,
-          padding: 0,
-          borderRadius: 10,
-          border: "4px solid black",
-          boxShadow: "0 0 20px #ccc",
-        }}
-        center={[40.73061, -73.935242]}
-        zoom={13}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {createAntennas()}
-      </MapContainer>
+      <DynamicMap/>
     </>
   );
 }
