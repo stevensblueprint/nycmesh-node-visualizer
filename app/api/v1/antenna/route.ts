69e00a3f95a3971dd5900b1b7f2c629766e2a285
Jonathan Memoli <jonomemoli3@gmail.com>
2024-04-19 18:44:59 -0400

    Add Update Endpoints and Fix other endpoints for new schema (#189)
    * Modified update endpoint to change the playground_frequency
    
    Also updated the zod typechecking and DELETE endpoint to work with new schema
    
    * feat: Add endpoint to update multiple antenna playground_frequencies given a list of ids
    
    * Added new labeledFrequencies type and changed request body sctucture accordingly
    
    * Fixed merge error

--- app/api/v1/antenna/route.ts
+++ app/api/v1/antenna/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from 'next/server';
 import { pool } from '../connection';
+import { isAntenna, Antenna } from '@/app/api/v1/antenna/validate';
+import StatusError from '@/app/api/(utils)/StatusError';
+import { labeledFrequencies } from '@/app/types';
 
 export async function GET() {
   try {
@@ -12,5 +15,55 @@ export async function GET() {
     let message = 'Internal server error';
     if (error instanceof Error) message = error.message;
     return NextResponse.json({ message }, { status: 500 });
+  }
+}
+
+// request body should be:
+// {
+//   "dataArray": [
+//     {labeledFrequencies}
+//   ]
+// }
+export async function PUT(request: Request) {
+  const updatedAntennas: Antenna[] = [];
+  try {
+    const { dataArray }: { dataArray: labeledFrequencies[] } =
+      (await request.json()) as { dataArray: labeledFrequencies[] };
+    const client = await pool.connect();
+
+    for (const { id, frequency } of dataArray) {
+      const query = `
+      UPDATE Antennas
+      SET playground_frequency = $1
+      WHERE id = $2
+      RETURNING *
+      `;
+      const result = await client.query(query, [frequency, id]);
+      if (result.rowCount === 0) {
+        throw new StatusError(404, `Antenna with ${id} does not exist.`);
+      }
+      if (!isAntenna(result.rows[0])) {
+        throw new StatusError(
+          500,
+          'Antenna must be updated with correct data types/values'
+        );
+      }
+      const updatedAntenna = result.rows[0];
+      updatedAntennas.push(updatedAntenna);
+    }
+    client.release();
+
+    return NextResponse.json(updatedAntennas, { status: 200 });
+  } catch (error) {
+    let message = 'Internal Server Error';
+    let status = 500;
+    if (typeof error === 'object') {
+      if (error && 'message' in error && typeof error.message === 'string')
+        message = error.message;
+      if (error && 'status' in error && typeof error.status === 'number')
+        status = error.status;
+    }
+
+    return NextResponse.json({ message }, { status });
   }
 }
